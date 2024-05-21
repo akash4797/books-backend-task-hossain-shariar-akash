182f4a41cc752059dace00445959cb91894393aa
Shariar Akash <55664045+akash4797@users.noreply.github.com>
2024-05-22 05:46:30 +0600

    remove dist

---
+++ dist/graphql/resolvers/authors.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const db_1 = require("../../lib/db");
+exports.default = {
+    Query: {
+        getauthors: async () => {
+            return await db_1.prisma.author.findMany();
+        },
+        getauthor: async (_, { id }) => {
+            return await db_1.prisma.author.findUnique({
+                where: {
+                    id: id,
+                },
+            });
+        },
+    },
+    Mutation: {
+        addauthor: async (_, { authorinput, }) => {
+            const { name, email, bio, birthdate } = authorinput;
+            return await db_1.prisma.author.create({
+                data: {
+                    name: name,
+                    email: email,
+                    bio: bio,
+                    birthdate: birthdate,
+                },
+            });
+        },
+    },
+};
